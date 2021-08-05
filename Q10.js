1acebcd580339b71aa92c3bd62f481d641eb76a6
GVKeerthana <88425547+GVKeerthana@users.noreply.github.com>
2021-08-05 16:16:40 +0530

    Q10

--- Q10.js
+++ Q10.js
@@ -3,4 +3,4 @@ function Hero(name, level) {
     this.level = level;
 }
 
-//A constructor function is initialized with a number of parameters, which would be assigned as properties of this, referring to the function itself.
+A constructor function is initialized with a number of parameters, which would be assigned as properties of this, referring to the function itself.
