3bc3d1580ac78ca9b5b008cdadc3a8bb03aefc04
GVKeerthana <88425547+GVKeerthana@users.noreply.github.com>
2021-08-05 12:31:34 +0530

    Create Q8.js

---
+++ Q8.js
@@ -0,0 +1,18 @@
+const name = {
+    firstName: 'Philip',
+    lastName: 'Fry'
+};
+const details = {
+    job: 'Delivery Boy',
+    employer: 'Planet Express'
+};
+const character = Object.assign(name, details);
+
+console.log(character);
+
+//output: {
+//firstName: 'Philip',
+// lastName: 'Fry',
+//job: 'Delivery Boy',
+//employer: 'Planet Express'
+}
